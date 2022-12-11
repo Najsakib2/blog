82ad85a3966d439463b9f4b578423b1cd272f48f
sakib <nazmus.sakib.dev@gmail.com>
2022-12-11 18:37:32 +0600

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'blog';
+  title = 'style Binding';
+  user = ['sakib', 'nazil', 'rakib', 'nafis'];
+  bgColor = 'red';
+  ok = 'orange';
+  updateColor() {
+    this.ok = 'red';
+    this.bgColor = 'blue';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TodoModule } from './todo/todo.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, TodoModule],
+  declarations: [AppComponent, HeaderComponent],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
